388fe9d092a173e4be15f5c21ce09ecf4f6f9772
Rithik Bhatia <rithikbhatia12@gmail.com>
2022-02-07 21:37:46 +0400

    Added folder navigation

--- PlaylistManager/Views/PlaylistsListView.axaml.cs
+++ PlaylistManager/Views/PlaylistsListView.axaml.cs
@@ -1,6 +1,8 @@
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using PlaylistManager.Types;
 using PlaylistManager.UserControls;
@@ -11,45 +13,86 @@ namespace PlaylistManager.Views
 {
     public class PlaylistsListView : UserControl
     {
+        private readonly ViewModel viewModel;
+        
         public PlaylistsListView()
         {
             InitializeComponent();
-            DataContext = new ViewModel();
+            viewModel = new ViewModel();
+            DataContext = viewModel;
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
+
+        private void OnClick(object? sender, RoutedEventArgs e)
+        {
+            if (viewModel.CurrentManager != null)
+            {
+                var parentManager = viewModel.CurrentManager.Parent;
+                if (parentManager != null)
+                {
+                    viewModel.CurrentManager = parentManager;
+                }
+            }
+        }
         
+        private void OnDoubleTapped(object? sender, RoutedEventArgs e)
+        {
+            if (viewModel.SelectedPlaylistOrManager is {isPlaylist: false})
+            {
+                viewModel.CurrentManager = viewModel.SelectedPlaylistOrManager.playlistManager;
+            }
+        }
+
         public class ViewModel : ViewModelBase
         {
-            private readonly PlaylistLibUtils? playlistLibUtils = Locator.Current.GetService<PlaylistLibUtils>();
-            private PlaylistCoverViewModel? selectedPlaylist;
-
-            public ObservableCollection<PlaylistCoverViewModel> SearchResults { get; } = new();
+            private readonly PlaylistLibUtils? playlistLibUtils;
+            private BeatSaberPlaylistsLib.PlaylistManager? currentManager;
+            private PlaylistCoverViewModel? selectedPlaylistOrManager;
             
-            public PlaylistCoverViewModel? SelectedPlaylist
+            public ObservableCollection<PlaylistCoverViewModel> SearchResults { get; } = new();
+            public bool IsSubfolder => currentManager?.Parent != null;
+            public string FolderTitle => Path.GetFileName(currentManager?.PlaylistPath) ?? "";
+            
+            public BeatSaberPlaylistsLib.PlaylistManager? CurrentManager
             {
-                get => selectedPlaylist;
+                get => currentManager;
                 set
                 {
-                    selectedPlaylist = value;
+                    currentManager = value;
+                    _ = LoadPlaylistsAsync();
+                    NotifyPropertyChanged(nameof(IsSubfolder));
+                    NotifyPropertyChanged(nameof(FolderTitle));
+                }
+            }
+            
+            public PlaylistCoverViewModel? SelectedPlaylistOrManager
+            {
+                get => selectedPlaylistOrManager;
+                set
+                {
+                    selectedPlaylistOrManager = value;
                     NotifyPropertyChanged();
                 }
             }
             
             public ViewModel()
             {
-                Task.Run(LoadPlaylistsAsync);
+                playlistLibUtils = Locator.Current.GetService<PlaylistLibUtils>();
+                currentManager = playlistLibUtils?.PlaylistManager;
+                _ = LoadPlaylistsAsync();
             }
 
             private async Task LoadPlaylistsAsync()
             {
-                if (playlistLibUtils != null)
+                if (playlistLibUtils != null && currentManager != null)
                 {
-                    var folders = playlistLibUtils.PlaylistManager.GetChildManagers();
-                    var playlists = await playlistLibUtils.GetPlaylistsAsync(playlistLibUtils.PlaylistManager);
+                    SearchResults.Clear();
+                    var folders = currentManager.GetChildManagers();
+                    var playlists = await playlistLibUtils.GetPlaylistsAsync(currentManager);
                     foreach (var folder in folders)
                     {
                         SearchResults.Add(new PlaylistCoverViewModel(folder));
