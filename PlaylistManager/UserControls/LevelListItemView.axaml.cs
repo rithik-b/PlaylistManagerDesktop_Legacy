d6aeae6e0bc3670a2b930b5d2dbaf30242172dc7
Rithik Bhatia <rithikbhatia12@gmail.com>
2022-02-13 07:08:26 +0400

    Showing highlighted difficulties

--- PlaylistManager/UserControls/LevelListItemView.axaml.cs
+++ PlaylistManager/UserControls/LevelListItemView.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -45,16 +46,16 @@ namespace PlaylistManager.UserControls
         private CoverImageLoader? coverImageLoader;
         private string? selectedCharacteristic;
         private List<Difficulty>? difficulties;
-
+        
         public LevelListItemViewModel(PlaylistSongWrapper playlistSong)
         {
             this.playlistSong = playlistSong;
-
+            
             foreach (var characteristic in playlistSong.customLevelData.Difficulties.Keys)
             {
                 Characteristics.Add(characteristic);
             }
-
+            
             SelectedCharacteristic = Characteristics.FirstOrDefault();
         }
 
@@ -89,7 +90,42 @@ namespace PlaylistManager.UserControls
                 if (selectedCharacteristic != null && playlistSong.customLevelData.Difficulties.TryGetValue(selectedCharacteristic, out var difficulties))
                 {
                     this.difficulties = difficulties;
+
+                    EasyHighlighted = false;
+                    NormalHighlighted = false;
+                    HardHighlighted = false;
+                    ExpertHighlighted = false;
+                    ExpertPlusHighlighted = false;
+                    
+                    if (playlistSong.playlistSong.Difficulties != null)
+                    {
+                        foreach (var difficulty in playlistSong.playlistSong.Difficulties)
+                        {
+                            if (difficulty.Characteristic.Equals(SelectedCharacteristic, StringComparison.OrdinalIgnoreCase))
+                            {
+                                switch (difficulty.DifficultyValue)
+                                {
+                                    case (int)Difficulty.Easy:
+                                        EasyHighlighted = true;
+                                        break;
+                                    case (int)Difficulty.Normal:
+                                        NormalHighlighted = true;
+                                        break;
+                                    case (int)Difficulty.Hard:
+                                        HardHighlighted = true;
+                                        break;
+                                    case (int)Difficulty.Expert:
+                                        ExpertHighlighted = true;
+                                        break;
+                                    case (int)Difficulty.ExpertPlus:
+                                        ExpertPlusHighlighted = true;
+                                        break;
+                                }
+                            }
+                        }
+                    }
                 }
+                
                 NotifyPropertyChanged();
                 NotifyPropertyChanged(nameof(IsEasy));
                 NotifyPropertyChanged(nameof(IsNormal));
@@ -98,11 +134,168 @@ namespace PlaylistManager.UserControls
                 NotifyPropertyChanged(nameof(IsExpertPlus));
             }
         }
+        public bool MultipleCharacteristics => playlistSong.customLevelData.Difficulties.Keys.Count > 1;
+
+        #region Difficulties
+        
+        private const string kEasyBackground = "#8067AC5B";
+        private const string kNormalBackground = "#804795EC";
+        private const string kHardBackground = "#80F19C38";
+        private const string kExpertBackground = "#80E15241";
+        private const string kExpertPlusBackground = "#808F34AA";
+        
+        private const string kEasyHighlightedBackground = "#FF67AC5B";
+        private const string kNormalHighlightedBackground = "#FF4795EC";
+        private const string kHardHighlightedBackground = "#FFF19C38";
+        private const string kExpertHighlightedBackground = "#FFE15241";
+        private const string kExpertPlusHighlightedBackground = "#FF8F34AA";
+
         public bool IsEasy => difficulties != null && difficulties.Contains(Difficulty.Easy);
         public bool IsNormal => difficulties != null && difficulties.Contains(Difficulty.Normal);
         public bool IsHard => difficulties != null && difficulties.Contains(Difficulty.Hard);
         public bool IsExpert => difficulties != null && difficulties.Contains(Difficulty.Expert);
         public bool IsExpertPlus => difficulties != null && difficulties.Contains(Difficulty.ExpertPlus);
+        
+        private bool easyHighlighted;
+        private bool EasyHighlighted
+        {
+            get => easyHighlighted;
+            set
+            {
+                if (easyHighlighted != value)
+                {
+                    easyHighlighted = value;
+                    if (value)
+                    {
+                        HighlightDifficulty(Difficulty.Easy);
+                    }
+                    else
+                    {
+                        UnHighlightDifficulty(Difficulty.Easy);
+                    }
+                    NotifyPropertyChanged(nameof(EasyBackground));
+                }
+            }
+        }
+        
+        private bool normalHighlighted;
+        private bool NormalHighlighted
+        {
+            get => normalHighlighted;
+            set
+            {
+                if (normalHighlighted != value)
+                {
+                    normalHighlighted = value;
+                    if (value)
+                    {
+                        HighlightDifficulty(Difficulty.Normal);
+                    }
+                    else
+                    {
+                        UnHighlightDifficulty(Difficulty.Normal);
+                    }
+                    NotifyPropertyChanged(nameof(NormalBackground));
+                }
+            }
+        }
+        
+        private bool hardHighlighted;
+        private bool HardHighlighted
+        {
+            get => hardHighlighted;
+            set
+            {
+                if (hardHighlighted != value)
+                {
+                    hardHighlighted = value;
+                    if (value)
+                    {
+                        HighlightDifficulty(Difficulty.Hard);
+                    }
+                    else
+                    {
+                        UnHighlightDifficulty(Difficulty.Hard);
+                    }
+                    NotifyPropertyChanged(nameof(HardBackground));
+                }
+            }
+        }
+        
+        private bool expertHighlighted;
+        private bool ExpertHighlighted
+        {
+            get => expertHighlighted;
+            set
+            {
+                if (expertHighlighted != value)
+                {
+                    expertHighlighted = value;
+                    if (value)
+                    {
+                        HighlightDifficulty(Difficulty.Expert);
+                    }
+                    else
+                    {
+                        UnHighlightDifficulty(Difficulty.Expert);
+                    }
+                    NotifyPropertyChanged(nameof(ExpertBackground));
+                }
+            }
+        }
+        
+        private bool expertPlusHighlighted;
+        private bool ExpertPlusHighlighted
+        {
+            get => expertPlusHighlighted;
+            set
+            {
+                if (expertPlusHighlighted != value)
+                {
+                    expertPlusHighlighted = value;
+                    if (value)
+                    {
+                        HighlightDifficulty(Difficulty.ExpertPlus);
+                    }
+                    else
+                    {
+                        UnHighlightDifficulty(Difficulty.ExpertPlus);
+                    }
+                    NotifyPropertyChanged(nameof(ExpertPlusBackground));
+                }
+            }
+        }
+
+        public string EasyBackground => EasyHighlighted ? kEasyHighlightedBackground : kEasyBackground;
+        public string NormalBackground => NormalHighlighted ? kNormalHighlightedBackground : kNormalBackground;
+        public string HardBackground => HardHighlighted ? kHardHighlightedBackground : kHardBackground;
+        public string ExpertBackground => ExpertHighlighted ? kExpertHighlightedBackground : kExpertBackground;
+        public string ExpertPlusBackground => ExpertPlusHighlighted ? kExpertPlusHighlightedBackground :  kExpertPlusBackground;
+
+        private void HighlightDifficulty(Difficulty difficulty)
+        {
+            if (SelectedCharacteristic != null)
+            {
+                playlistSong.playlistSong.Difficulties ??= new List<BeatSaberPlaylistsLib.Types.Difficulty>();
+                var diffToHighlight = new BeatSaberPlaylistsLib.Types.Difficulty();
+                diffToHighlight.Characteristic = SelectedCharacteristic;
+                diffToHighlight.Name = difficulty.ToString();
+                playlistSong.playlistSong.Difficulties.Add(diffToHighlight);
+            }
+        }
+        
+        private void UnHighlightDifficulty(Difficulty difficulty)
+        {
+            if (playlistSong.playlistSong.Difficulties != null && SelectedCharacteristic != null)
+            {
+                var diffToDelete = playlistSong.playlistSong.Difficulties.FindIndex(d =>
+                    d.Characteristic.Equals(SelectedCharacteristic, StringComparison.OrdinalIgnoreCase) &&
+                    d.DifficultyValue == (int) difficulty);
+                playlistSong.playlistSong.Difficulties.RemoveAt(diffToDelete);
+            }
+        }
+
+        #endregion
 
         private async Task LoadCoverAsync()
         {
