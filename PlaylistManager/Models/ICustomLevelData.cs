15d9cf25df2faf6e0f5f56264770968b0eac0a52
PixelBoom <rithikbhatia12@gmail.com>
2022-02-27 15:06:33 +0400

    Now showing every level in playlist, even unknown
    Unknown levels (i.e. ones with no ICustomLevelData) are represented with as much info as possible from the PlaylistSong. Now, they can be removed or rearranged as needed by the user.

--- PlaylistManager/Models/ICustomLevelData.cs
+++ PlaylistManager/Models/ICustomLevelData.cs
@@ -5,23 +5,8 @@ using Avalonia.Media.Imaging;
 
 namespace PlaylistManager.Models
 {
-    public interface ICustomLevelData
+    public interface ICustomLevelData : ILevelData
     {
-        /// <summary>
-        /// Name of the song
-        /// </summary>
-        public string SongName { get; }
-        
-        /// <summary>
-        /// Sub name of the song
-        /// </summary>
-        public string SongSubName { get; }
-        
-        /// <summary>
-        /// Artist of the song
-        /// </summary>
-        public string SongAuthorName { get; }
-        
         /// <summary>
         /// Mapper of the level
         /// </summary>
@@ -38,19 +23,9 @@ namespace PlaylistManager.Models
         public bool Downloaded { get; }
 
         /// <summary>
-        /// A dictionary that maps from Characteristic to a List of Difficulties in that Characteristic
-        /// </summary>
-        public Dictionary<string, List<Difficulty>> Difficulties { get; }
-        
-        /// <summary>
         /// Asynchronously get the BeatSaver ID of the level
         /// </summary>
         public Task<string?> GetKeyAsync();
-        
-        /// <summary>
-        /// Asynchronously loads and parses the cover image of a level
-        /// </summary>
-        public Task<Bitmap?> GetCoverImageAsync(CancellationToken? cancellationToken = null);
     }
     
     public enum Difficulty
--- PlaylistManager/Models/PlaylistSongWrapper.cs
+++ PlaylistManager/Models/PlaylistSongWrapper.cs
@@ -1,19 +1,66 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
 using BeatSaberPlaylistsLib.Types;
+using PlaylistManager.Utilities;
 
 namespace PlaylistManager.Models
 {
     /// <summary>
     /// A wrapper class that combines PlaylistSong with ICustomLevelData
     /// </summary>
-    public class PlaylistSongWrapper
+    public class PlaylistSongWrapper : ILevelData
     {
         public readonly IPlaylistSong playlistSong;
-        public readonly ICustomLevelData customLevelData;
+        private readonly ICustomLevelData? customLevelData;
         
-        public PlaylistSongWrapper(IPlaylistSong playlistSong, ICustomLevelData customLevelData)
+        public string SongName
         {
-            this.playlistSong = playlistSong;
+            get
+            {
+                if (customLevelData != null)
+                {
+                    return customLevelData.SongName;
+                }
+
+                if (playlistSong.Name != null)
+                {
+                    return playlistSong.Name;
+                }
+
+                return playlistSong.TryGetIdentifierForPlaylistSong(out var identifier, out var identifierType)
+                    ? identifier : "Unknown";
+            }
+        }
+        public string SongSubName => customLevelData?.SongSubName ?? "";
+        public string SongAuthorName => customLevelData != null ? customLevelData.SongAuthorName : "Unknown";
+        public string LevelAuthorName => customLevelData != null 
+            ? customLevelData.LevelAuthorName 
+            : playlistSong.LevelAuthorName ?? "Unknown";
+        public bool Downloaded => customLevelData?.Downloaded ?? false;
+        public Dictionary<string, List<Difficulty>> Difficulties => customLevelData?.Difficulties ?? new Dictionary<string, List<Difficulty>>();
+        
+        public PlaylistSongWrapper(IPlaylistSong playlistSong) => this.playlistSong = playlistSong;
+        public PlaylistSongWrapper(IPlaylistSong playlistSong, ICustomLevelData customLevelData) : this(playlistSong) =>
             this.customLevelData = customLevelData;
+        
+        public async Task<string?> GetKeyAsync()
+        {
+            if (customLevelData != null)
+            {
+                return await customLevelData.GetKeyAsync();
+            }
+            return null;
+        }
+
+        public async Task<Bitmap?> GetCoverImageAsync(CancellationToken? cancellationToken = null)
+        {
+            if (customLevelData != null)
+            {
+                return await customLevelData.GetCoverImageAsync(cancellationToken);
+            }
+            return null;
         }
     }
 }
